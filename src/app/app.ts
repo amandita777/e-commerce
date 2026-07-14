3f02ca40a56082358209e7718be81b64c2391f04
Amanda Freitas <freitasamanda4321@gmail.com>
2026-07-14 17:12:52 -0300

    adicionar novo imports ListaProdutos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal} from '@angular/core';
 //import { RouterOutlet } from '@angular/router'; // Remove importação do RouterOutlet, pois não está sendo utilizado no momento
 import { Produto } from './components/produto/produto'; // Importa o produto para dentro do app
+import { ListaProdutos } from './components/lista-produtos/lista-produtos'; //adicionar o import
 
 @Component({
   selector: 'app-root',
-  imports: [Produto],
+  imports: [ListaProdutos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
