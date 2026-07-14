273a875162d020d4f62a6a00e46a8c1e2d87cb32
Amanda Freitas <freitasamanda4321@gmail.com>
2026-07-14 14:36:10 -0300

    feat(Produto):adicionar pipes para formatação de nome e preços

--- src/app/components/produto/produto.ts
+++ src/app/components/produto/produto.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
+import {UpperCasePipe, CurrencyPipe} from '@angular/common';
+import {PrecoFormatadoPipe} from '../../pipes/preco-formatado-pipe';
 
 @Component({
   selector: 'app-produto',
-  imports: [],
+  imports: [UpperCasePipe, PrecoFormatadoPipe],
   templateUrl: './produto.html',
   styleUrl: './produto.css',
 })
 export class Produto {
-  produto = 'Notebook';
-  preco = 3000;
-  mostrarPreco = true;
-  mostrarProduto = true;
+  //produto = 'Notebook';
+  //preco = 3000;
+  //mostrarPreco = true;
+  //mostrarProduto = true;
+  produtos = [
+    {produto: 'Mouse', preco: 100},
+    {produto: 'Desktop', preco: 499.99},
+    {produto: 'Caixa de Som', preco: 5200}
+  ];
 }
