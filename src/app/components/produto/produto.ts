591442dd1460551c2fed616af60e921d4ac82258
Amanda Freitas <freitasamanda4321@gmail.com>
2026-07-15 13:57:27 -0300

    feat(Produto): configurar component para receber nome e preço

--- src/app/components/produto/produto.ts
+++ src/app/components/produto/produto.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
 import {UpperCasePipe, CurrencyPipe} from '@angular/common';
 import {PrecoFormatadoPipe} from '../../pipes/preco-formatado-pipe';
 
@@ -9,13 +9,6 @@ import {PrecoFormatadoPipe} from '../../pipes/preco-formatado-pipe';
   styleUrl: './produto.css',
 })
 export class Produto {
-  //produto = 'Notebook';
-  //preco = 3000;
-  //mostrarPreco = true;
-  //mostrarProduto = true;
-  produtos = [
-    {produto: 'Mouse', preco: 100},
-    {produto: 'Desktop', preco: 499.99},
-    {produto: 'Caixa de Som', preco: 5200}
-  ];
+  @Input() nome: string = '';
+  @Input() preco: number = 0;
 }
