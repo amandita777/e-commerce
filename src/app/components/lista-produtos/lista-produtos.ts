2eafe1b613d6c03f583bbf6edb53a10db1907971
Amanda Freitas <freitasamanda4321@gmail.com>
2026-07-15 13:53:44 -0300

    feat(Lista-Produtos): adicionar lista de produtos e importar component Produto

--- src/app/components/lista-produtos/lista-produtos.ts
+++ src/app/components/lista-produtos/lista-produtos.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { Produto } from '../produto/produto';
 
 @Component({
   selector: 'app-lista-produtos',
-  imports: [],
+  imports: [Produto],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
-export class ListaProdutos {}
+export class ListaProdutos {
+  produtos = [
+    {nome: 'Teclado', preco: 59.99},
+    {nome: 'Mouse', preco: 89.99},
+    {nome: 'Monitor', preco: 899.99},
+    {nome: 'Disktop Gamer', preco: 200},
+    {nome: 'Headset Gamer', preco: 999.99}
+  ];
+}
