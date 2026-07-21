02b3ab1dedbba826758237438f950adb4cc31da6
Amanda Freitas <freitasamanda4321@gmail.com>
2026-07-21 17:16:31 -0300

    feat(lista-produto): implementa roteamento da aplicação com LazyLoading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,46 @@
-import { Routes } from '@angular/router';
-import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
-import { Carrinho } from './features/carrinho/carrinho/carrinho';
+// // import { Routes } from '@angular/router';
+// // //import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos'; impot legado
+// // import { Carrinho } from './features/carrinho/carrinho/carrinho';
+// // import { Home } from './features/home/home/home';
 
+// // export const routes: Routes = [
+// //     {
+// //         path: '',
+// //         component: Home,
+// //     },
+
+// //     //Código legado Router
+// //     // {
+// //     //     path: 'produtos',
+// //     //     component: ListaProdutos,
+// //     // },
+    
+// //     {
+// //         path: 'carrinho',
+// //         component: Carrinho,
+// //     },
+// // ];
+
+//! código final LazyLoading e LoadComponent
+import { Routes } from "@angular/router";
 export const routes: Routes = [
     {
-        path: '',
-        component: ListaProdutos,
+        path:'',
+        loadComponent: () =>
+            import('./features/home/home/home').then((m) => m.Home),
+    },
+    {
+        path: 'produtos',
+        loadComponent: () =>
+            import('./features/produtos/lista-produtos/lista-produtos').then((m) => m.ListaProdutos),
     },
     {
         path: 'carrinho',
-        component: Carrinho,
+        loadComponent: () =>
+            import('./features/carrinho/carrinho/carrinho').then((m) => m.Carrinho),
+    },
+    {
+        path: '**',
+        redirectTo: '',
     },
 ];
