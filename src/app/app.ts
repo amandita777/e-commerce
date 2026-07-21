2edadf8b987fe0ab8e549080b9878265617b08cc
Amanda Freitas <freitasamanda4321@gmail.com>
2026-07-21 17:17:55 -0300

    feat(lista-produto): adiciona configuração de navegação e nome da loja

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,5 @@
 import { Component, signal} from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
-//import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
 
 @Component({
   selector: 'app-root',
