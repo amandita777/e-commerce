5519be17a3425aea2ab6b3a37a3528e28173c730
Amanda Freitas <freitasamanda4321@gmail.com>
2026-07-22 16:12:28 -0300

    feat: proteger rota do carrrinho com authGuard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -21,8 +21,10 @@
 // //     },
 // // ];
 
-//! código final LazyLoading e LoadComponent
+//! Importações 
 import { Routes } from "@angular/router";
+import { authGuard } from "./core/auth.guard";
+
 export const routes: Routes = [
     {
         path:'',
@@ -36,6 +38,7 @@ export const routes: Routes = [
     },
     {
         path: 'carrinho',
+        canActivate: [authGuard],
         loadComponent: () =>
             import('./features/carrinho/carrinho/carrinho').then((m) => m.Carrinho),
     },
