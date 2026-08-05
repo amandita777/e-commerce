5c64a9a3dd74ebd17e83bc6510bcfba45b58ccd4
Amanda Freitas <freitasamanda4321@gmail.com>
2026-08-05 16:21:08 -0300

    feat: app.html, adiciona estilos para container principal e bloco de controle.. app.ts import Header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,16 @@
 import { Component, signal} from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { usuarioLogado, login, logout } from './core/auth';
+import { Header } from './shared/layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
-  nomeLoja = 'TecnoMundo';
   usuarioLogado = usuarioLogado;
   login = login;
   logout = logout;
