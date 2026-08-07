6b42c7da1659782e1cafdc31cd8e17a224f15b5d
Amanda Freitas <freitasamanda4321@gmail.com>
2026-08-07 14:12:26 -0300

    fix: comflitos resolvidos

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,14 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router'
 import { usuarioLogado, login, logout } from './core/auth';
 import { Header } from './shared/layout/header/header';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-<<<<<<< HEAD
-  imports: [RouterOutlet, RouterLink, Header],
-=======
-  imports: [RouterOutlet, Header],
->>>>>>> 7c2e94e028043275bca6e5fe1d1a3b164cddd3f6
+  imports: [RouterOutlet, RouterLink, Header, MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/shared/layout/header/header.ts
+++ src/app/shared/layout/header/header.ts
@@ -1,14 +1,4 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-import { MatToolbarModule} from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
-
-@Component({
-  selector: 'app-header',
-  imports: [ MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
-=======
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -20,16 +10,12 @@ import  { CarrinhoService } from '../../../core/service/carrinho.service';
 @Component({
   selector: 'app-header',
   imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink, UpperCasePipe],
->>>>>>> 7c2e94e028043275bca6e5fe1d1a3b164cddd3f6
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
   nomeLoja = 'TecnoMundo';
-<<<<<<< HEAD
-=======
 
   private carrinhoService = inject(CarrinhoService);
   quantidade = this.carrinhoService.quantidadeItens;
->>>>>>> 7c2e94e028043275bca6e5fe1d1a3b164cddd3f6
 }
