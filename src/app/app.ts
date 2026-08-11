80905e0260ab2af6d3e64920ec319785a11d8a0c
Amanda Freitas <freitasamanda4321@gmail.com>
2026-08-11 17:02:09 -0300

    feat: remover funções não utilizadas

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,6 @@
 import { Component, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router'
-import { usuarioLogado, login, logout } from './core/auth';
 import { Header } from './shared/layout/header/header';
 import { MatButtonModule } from '@angular/material/button';
 
@@ -13,7 +12,4 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class App {
   protected readonly title = signal('e-commerce');
-  usuarioLogado = usuarioLogado;
-  login = login;
-  logout = logout;
 }
