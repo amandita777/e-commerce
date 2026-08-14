8dc708d273879010961e63d3cbb20d7bef47528d
Amanda Freitas <freitasamanda4321@gmail.com>
2026-08-14 16:13:08 -0300

    feat: ajustar botão e incluir imports necessários

--- src/app/features/acesso-negado/acesso-negado/acesso-negado.ts
+++ src/app/features/acesso-negado/acesso-negado/acesso-negado.ts
@@ -1,14 +1,23 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../../core/service/auth.service';
+import { Router } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-acesso-negado',
-  imports: [RouterLink],
+  imports: [RouterLink, MatButtonModule],
   templateUrl: './acesso-negado.html',
   styleUrl: './acesso-negado.css',
 })
 export class AcessoNegado {
   private authService = inject(AuthService); //! Teste em produção
-  sair = this.authService.logout(); //! Teste em produção
+  private router = inject(Router);
+
+  sair(){
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
+    return;
+  }
+  
 }
