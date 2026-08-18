c7b4becb39bbdec6fdb54db019b39f7b181e4e51
Amanda Freitas <freitasamanda4321@gmail.com>
2026-08-18 17:13:29 -0300

    feat: adicionar imports, removerItem, limparCarrinho e cancelarCompra

--- src/app/features/carrinho/carrinho/carrinho.ts
+++ src/app/features/carrinho/carrinho/carrinho.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { CarrinhoFacade } from '../../../core/facades/carrinho.facade';
+import { AuthFacade } from '../../../core/facades/auth.facade';
+import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 
 @Component({
   selector: 'app-carrinho',
-  imports: [],
+  imports: [RouterLink, MatButtonModule, PrecoFormatadoPipe],
   templateUrl: './carrinho.html',
   styleUrl: './carrinho.css',
 })
 export class Carrinho {
-  //Lógica do carrinho virá em breve
-  //Por ora, apenas exibe a página de carrinho 
+public carrinhoFacade = inject(CarrinhoFacade);
+private router = inject(Router);
+private authFacade = inject(AuthFacade);
+
+removerItem(rmvItem: number){
+  this.carrinhoFacade.limparCarrinho();
+}
+
+limparCarrinho(){
+  this.carrinhoFacade.limparCarrinho();
+}
+
+cancelarCompra(){
+  this.authFacade.sair();
+  this.carrinhoFacade.limparCarrinho();
+  this.router.navigateByUrl('/login');
+}
+
 }
