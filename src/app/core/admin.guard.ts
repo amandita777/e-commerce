afbefcb486d9365e537d34251a3c090fe9396da4
Amanda Freitas <freitasamanda4321@gmail.com>
2026-08-18 13:30:52 -0300

    feat: adicionar nova atualização e remover a antiga

--- src/app/core/admin.guard.ts
+++ src/app/core/admin.guard.ts
@@ -1,18 +1,18 @@
 import { inject } from "@angular/core";
 import { Router } from "@angular/router";
 import { CanActivateFn } from "@angular/router";
-import { AuthService } from "./service/auth.service";
+import { AuthFacade } from "./facades/auth.facade";
 
 export const adminGuard: CanActivateFn = () => {
     const router = inject(Router);
-    const authService = inject(AuthService);
+    const authFacade = inject(AuthFacade);
 
     //! -1) Verificar se o Usuário está logado
-    if (!authService.usurioLogado()) {
+    if (!authFacade.usuarioLogado()) {
         return router.createUrlTree(['/login']);
     }
     //! -2) Verificar se o usuário atual (logado), se tem perfil adm
-    if (!authService.admin()) {
+    if (!authFacade.admin()) {
         return router.createUrlTree(['/acesso-negado'])
     }
     //! -3) Se o usuário estiver logado e for adm = ACESSO LIBERADO
