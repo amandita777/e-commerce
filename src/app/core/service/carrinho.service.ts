6f192f9b8a37d8b9c649d813e7d1820301e91241
Amanda Freitas <freitasamanda4321@gmail.com>
2026-08-20 16:03:42 -0300

    refector: reescreve totalmente substituindo o código anterior

--- src/app/core/service/carrinho.service.ts
+++ src/app/core/service/carrinho.service.ts
@@ -1,81 +1,65 @@
-import { Injectable } from "@angular/core";
-import { signal } from "@angular/core";
-import { computed } from "@angular/core"; 
-import { effect } from "@angular/core";
-import { inject } from "@angular/core";
-import { PLATFORM_ID } from "@angular/core";
-import { ItemCarrinho } from "../models/item-carrinho";
-import { isPlatformBrowser } from "@angular/common";
+import { isPlatformBrowser } from '@angular/common';
+import { Injectable, signal, computed, effect, inject, PLATFORM_ID } from '@angular/core';
+
+import { ItemCarrinho } from '../models/item-carrinho';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
+export class CarrinhoService {
+  private platformId = inject(PLATFORM_ID);
+  private readonly chaveStorage = 'minha-loja-carrinho';
+  private carrinho = signal<ItemCarrinho[]>(this.carregarCarrinhoSalvo());
 
-export class CarrinhoService{
+  // SELECTORS
+  itens = computed(() => this.carrinho());
+  quantidadeItens = computed(() => this.carrinho().length);
+  totalItens = computed(() => this.carrinho().reduce((total, item) => total + item.preco, 0));
+  carrinhoVazio = computed(() => this.carrinho().length === 0);
 
-//! Estado Global
-private carrinho = signal<ItemCarrinho[]>(this.carregarcarrinhoSalvo());
-
-//! Seletores
-itens = computed(() => this.carrinho());
-quantidadeItens = computed(() => this.carrinho().length); //! Quantidade de itens do carrinho
-totalItens = computed(() => 
-    this.carrinho().reduce((total, item) => total + item.preco,0)
-);
-
-carrinhoVazio = computed(() => this.carrinho().length === 0);
-
-//! ======== PERSISTÊNCIA CARRINHO =========
-
- private platformId = inject(PLATFORM_ID);
-
-   //!Chave de recuperação Localstorage
-    private readonly chaveStorage = 'carrinho-storage';
-
-constructor(){
-    effect(() =>{
-        this.salvarCarrinho(this.carrinho());
+  constructor() {
+    // Sempre que o carrinho mudar, a lista atualizada será persistida.
+    effect(() => {
+      this.salvarCarrinho(this.carrinho());
     });
-}
+  }
 
-private estaNoNavegador(): boolean{
-    return isPlatformBrowser(this.platformId);
-}
+  // ACTIONS
+  adicionar(produto: ItemCarrinho) {
+    this.carrinho.update((lista) => [...lista, produto]);
+  }
 
-private carregarcarrinhoSalvo(): ItemCarrinho [] {
-    if(!this.estaNoNavegador()){
-        return[];
-    } 
-    const dadosSalvos = localStorage.getItem(this.chaveStorage);
+  removerItem(rmvItem: number) {
+    this.carrinho.update((listaAtual) => listaAtual.filter((_, index) => index !== rmvItem));
+  }
 
-    if(!dadosSalvos){
-        return[];
-    }
-
-    try {
-        return JSON.parse(dadosSalvos) as ItemCarrinho[];
-    }catch{
-        return[];
-    }
-}
-
-private salvarCarrinho(item: ItemCarrinho[]){
-    if(!this.estaNoNavegador()){
-        return;
-    }
-    localStorage.setItem(this.chaveStorage, JSON.stringify(item));
-}
-
-// TODO: Ações
-adicionar(produto:ItemCarrinho){
-    this.carrinho.update(lista => [...lista, produto])
-}
-limpar() {
+  limpar() {
     this.carrinho.set([]);
-}
+  }
 
-removerItem(rmvItem:number){
-    this.carrinho.update((listaAtual) =>
-    listaAtual.filter((_, index) => index !== rmvItem));
-}
+  private estaNoNavegador(): boolean {
+    return isPlatformBrowser(this.platformId);
+  }
+
+  private carregarCarrinhoSalvo(): ItemCarrinho[] {
+    if (!this.estaNoNavegador()) {
+      return [];
+    }
+    const dadosSalvos = localStorage.getItem(this.chaveStorage);
+    if (!dadosSalvos) {
+      return [];
+    }
+    try {
+      return JSON.parse(dadosSalvos) as ItemCarrinho[];
+    } catch {
+      return [];
+    }
+  }
+
+  private salvarCarrinho(itens: ItemCarrinho[]) {
+    if (!this.estaNoNavegador()) {
+      return;
+    }
+    localStorage.setItem(this.chaveStorage, JSON.stringify(itens));
+  }
 }
