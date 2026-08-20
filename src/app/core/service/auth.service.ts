a4d94eba855ea4c7ae9576db02f4c9e5acded70f
Amanda Freitas <freitasamanda4321@gmail.com>
2026-08-20 13:48:09 -0300

    feat: implementar persistência da autenticação

--- src/app/core/service/auth.service.ts
+++ src/app/core/service/auth.service.ts
@@ -1,6 +1,9 @@
-import { Injectable } from "@angular/core";
+import { inject, Injectable } from "@angular/core";
 import { signal } from "@angular/core";
 import { computed } from "@angular/core";
+import { effect } from "@angular/core";
+import { PLATFORM_ID } from "@angular/core";
+import { isPlatformBrowser } from "@angular/common";
 
 type PerfilUsuario = 'admin' | 'usuario';
 
@@ -15,13 +18,21 @@ type Usuario = {
 
 export class AuthService {
 
-    private usuario = signal <Usuario | null>(null);
-    private tokenJwt = signal <string | null>(null);
+    private usuario = signal <Usuario | null>(this.carregarUsuarioSalvo());
+    private tokenJwt = signal <string | null>(this.carregarTokenSalvo());
 
     usuarioAtual = computed(() => this.usuario());
-    usurioLogado = computed(() => this.usuario() !== null);
     token = computed(() => this.tokenJwt());
     admin = computed(() => this.usuario()?.perfil === 'admin');
+
+    //!============== PERSISTÊNCIA AUTH ===================
+
+    private platformId = inject(PLATFORM_ID);
+    
+    private readonly chaveUsuario = 'usuario-storage'; 
+    private readonly chaveToken = 'token-storage';
+
+    usuarioLogado = computed (() => this.usuario() !== null && this.tokenJwt() !== null );
 
     login (email: string, senha: string): boolean{
         if(!email || !senha){
@@ -29,6 +40,12 @@ export class AuthService {
         }
     
     const perfil: PerfilUsuario = email === 'admin@email.com' ? 'admin' : 'usuario';    
+
+    const usuarioLogado: Usuario = {
+        email,
+        perfil,
+    };
+
     const tokenSimulado = 
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.' +
     'eyJzdWIiOiJhbHVub0B0ZXN0ZS5jb20iLCJwZXJmaWwiOiJ1c3VhcmlvIn0.' 
@@ -40,11 +57,18 @@ export class AuthService {
     });
 
     this.tokenJwt.set(tokenSimulado);
+    this.usuario.set(usuarioLogado);
+    
+    this.salvarAutenticacao(usuarioLogado, tokenSimulado);
+
     return true;
     }
     logout() {
         this.usuario.set(null);
         this.tokenJwt.set(null);
+
+        this.limparAutenticacaoSalva();
+
     }
     obterToken(): string | null {
         return this.tokenJwt();
@@ -52,4 +76,48 @@ export class AuthService {
     obterPerfil(): PerfilUsuario | null {
         return this.usuario()?.perfil ?? null;
     }
+
+    private estaNoNavegador(): boolean{
+        return isPlatformBrowser(this.platformId);
+    }
+
+    private carregarUsuarioSalvo(): Usuario | null {
+        if (!this.estaNoNavegador()){
+            return null;
+        }
+
+        const dadosSalvos = localStorage.getItem(this.chaveUsuario);
+
+        if(!dadosSalvos){
+            return null;
+        }try {
+            return JSON.parse(dadosSalvos) as Usuario;
+        }catch {
+            return null;
+        }    
+}
+private carregarTokenSalvo(): string | null {
+    if(!this.estaNoNavegador()){
+        return null;
+    }
+    return localStorage.getItem(this.chaveToken);  
+}
+private salvarAutenticacao(usuario: Usuario, token: string){
+
+    if(!this.estaNoNavegador()){
+        return;
+    }
+
+    localStorage.setItem(this.chaveUsuario, JSON.stringify(usuario));
+    localStorage.setItem(this.chaveToken, (token));
+}
+private limparAutenticacaoSalva(){
+
+    if(!this.estaNoNavegador()){
+        return;
+    }
+
+    localStorage.removeItem(this.chaveUsuario);
+    localStorage.removeItem(this.chaveToken);
+}
 }
